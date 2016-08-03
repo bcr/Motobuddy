c4a36c662620c350c6ae7479fec6af7473dc825e
Blake Ramsdell <blaker@gmail.com>
2016-08-03 00:34:52 -0700

    First cut of init code.

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_GPS.h>
+#include <Adafruit_NeoPixel.h>
+
+#define OLED_RESET_PIN      4       // Pin used for OLED reset signal
+#define OLED_I2C_ADDRESS    0x3C    // 0x3C (for the 128x32)
+#define DHT_PIN             2       // Pin which is connected to the DHT sensor.
+#define GPSSerial           Serial1 // what's the name of the hardware serial port?
+#define PIXEL_PIN           6       // Digital IO pin connected to the NeoPixels.
+#define PIXEL_COUNT         16      // Number of NeoPixels connected
+
+// http://platformio.org/lib/show/28/Adafruit-NeoPixel
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+
+// http://platformio.org/lib/show/135/Adafruit-SSD1306
+Adafruit_SSD1306 display(OLED_RESET_PIN);
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+// http://platformio.org/lib/show/18/Adafruit-DHT-Unified
+#define DHT_TYPE          DHT22     // DHT 22 (AM2302)
+DHT_Unified dht(DHT_PIN, DHT_TYPE);
+
+void setup()
+{
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+    display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS);  // initialize with the I2C addr
+    dht.begin();
+
+    // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+    GPS.begin(9600);
+    // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    // uncomment this line to turn on only the "minimum recommended" data
+    //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+    // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+    // the parser doesn't care about other sentences at this time
+    // Set the update rate
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+    // For the parsing code to work nicely and have time to sort thru the data, and
+    // print it out we don't suggest using anything higher than 1 Hz
+     
+    // Request updates on antenna status, comment out to keep quiet
+    GPS.sendCommand(PGCMD_ANTENNA);
+
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+    
+}
